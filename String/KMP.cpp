5ef1311a826c7ce3010c162fecac2696cd385e6c
Scar <565297712@qq.com>
2013-05-08 20:51:38 +0800

    Update and rename string.cpp to KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -28,25 +28,4 @@ int kmp(char *p, char *t){         //检查模式串p在主串t中是否存在
 	 }
 	return 0;
 }
-/*
-******最小表示法 ***********
 
-可用于判断字符串的循环同构。O（n）的复杂度。最小表示法 求出字符串所有循环同构中字典序最小的字符串的起点位置。
-例如 helloworld 的最小表示法为 dhelloworl 。所以起点为10
-*/
-iint minSub(char * a )             //返回字符串a的最小表示法的起点
-{
-	int i=0,j=1,len=strlen(a),k=0;
-	while(i<len && j<len &&k< len)
-	{
-		if(k==len) break;
-		if(i==j) j++;
-		int ni=i+k , nj=j+k;
-		if(ni>=len) ni-=len;
-		if(nj>=len) nj-=len;
-		if(a[ni] > a[nj]) { i+=k+1; k=0; }
-		else if(a[ni] < a[nj]) { j+=k+1; k=0; }
-		else 	k++;
-	}
-	return i;
-}
