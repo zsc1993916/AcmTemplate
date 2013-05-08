bc3e0e26acaad0397ebb7f33a2f5009a940b7b3a
Scar <bestscar1993@gmail.com>
2013-05-08 20:37:47 +0800

    add

---
+++ Header.cpp
@@ -0,0 +1,68 @@
+/****HEADER*****/
+#define LOCAL
+
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<utility>
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<functional>
+using namespace std;
+
+//define
+
+//typedef
+typedef long long LL;
+typedef double DB;
+typedef unsigned long long ULL;
+typedef vector<int> VI;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef vector<LL> VL;
+typedef vector<DB> VF;
+typedef set<int> SI;
+typedef set<string> SS;
+typedef map<int, int> MII;
+typedef map<string, int> MSI;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+
+//Constant
+const int MOD = int(1e9) + 7;
+const int INF = 0x3f3f3f3f;
+const DB EPS = 1e-9;
+const DB PI = acos(-1.0); //M_PI;
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+
+//template
+template<class T> inline T min(T a, T b, T c){return min(min(a, b), c);}
+template<class T> inline T max(T a, T b, T c){return max(max(a, b), c);}
+template<class T> inline T sqr(T a){return a*a;}
+template<class T> inline T cub(T a){return a*a*a;}
+
+
+/*****MAIN*****/
+
+int main(){
+
+#ifndef OJ
+	freopen("in.in", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+#endif
+
+
+	return 0;
+}
