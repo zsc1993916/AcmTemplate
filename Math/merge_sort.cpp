12c4ab7ef93907b7923214b122603e77882f8545
Scar <565297712@qq.com>
2013-07-23 16:05:54 +0800

    Update and rename add.cpp to merge_sort.cpp

---
+++ Math/merge_sort.cpp
@@ -0,0 +1,16 @@
+// tot为数组a[]区间[x,y)逆序数对数
+void merge_sort(int x,int y)
+{
+  int mid=x+(y-x)/2;
+	if(y-x>1){
+		merge_sort(x,mid);
+		merge_sort(mid,y);
+		int p=x,q=mid,i=x;
+		while(p<mid||q<y){
+			if(q>=y||(p<mid&&a[p]<=a[q]))
+				t[i++]=a[p++]; 
+			else{ t[i++]=a[q++]; tot+=mid-p; }
+		}
+		for(i=x;i<y;i++) a[i]=t[i];
+	}
+}
