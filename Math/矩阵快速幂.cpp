586ca0c41bab5d0f97750ac9a4a531ce90b29883
Scar <565297712@qq.com>
2013-07-24 22:52:04 +0800

    Update 矩阵快速幂.cpp

--- Math/矩阵快速幂.cpp
+++ Math/矩阵快速幂.cpp
@@ -1,42 +1,29 @@
-/*
- 矩阵快速幂，矩阵大小为N*N
- 小心溢出
-*/
-class Matrix {   
-public:
-    int m[N][N]; //二维数组存放矩阵
-    Matrix(){}  //对数组的初始化
-    void init(int num[N][N]){
-        for(int i=0;i<N;i++)
-            for(int j=0;j<N;j++)
-                m[i][j] = num[i][j];
-    }
-    //重载矩阵的乘法运算
-    friend Matrix operator*(Matrix &m1 ,Matrix &m2) {
-        int i,j,k;
-        Matrix t;
-        for (i=0;i<N; i++) {
-            for (j=0; j<N;j++) {
-                t.m[i][j]=0;
-                for(k=0;k<N;k++)
-                    t.m[i][j]+=(m1.m[i][k]* m2.m[k][j])%MOD;
-                t.m[i][j]%=MOD;
-           }
-        }
-        return t;
-    }
-    //矩阵的快速幂
-    friend Matrix quickpow(Matrix &M , int n){  //M是底数 返回M^n
-        Matrix ans;
-        for(int i=0;i<N;i++)  //初始化为单位矩阵
-            for(int j=0;j<N;j++)
-                    ans.m[i][j]= (i==j);
-        //快速幂
-        while(n){
-            if(n&1)  ans=ans*M;
-            n>>=1;
-            M=M*M;
-        }
-       return ans;
-    }
+/* 矩阵快速幂，矩阵最大为N*N,小心中间溢出
+  size 矩阵边长   MOD 模  */
+struct Matrix{
+    int a[N][N];
+    //乘法重载
+ Matrix operator * (const Matrix &b) const{
+		Matrix res;
+		for (int i=0; i<size; i++)
+			for (int j=0; j<size; j++) {
+				res.a[i][j]=0;
+				for (int k=0; k<size; k++){
+					res.a[i][j]+= (a[i][k] * b.a[k][j])%MOD;
+					res.a[i][j]%=MOD;
+				}
+			}
+		return res;
+	}
+	Matrix matrix_pow(int n){  //矩阵快速幂 a^n
+		Matrix res,x;
+		for (int i=0; i<size; i++){
+			for (int j=0; j<size; j++)
+				res.a[i][j]=0, x.a[i][j]=a[i][j];
+			res.a[i][i] = 1;
+		}
+		for(; n; n>>=1,x=x*x)
+			if(n&1) res=res*x;
+		return res;
+	}
 };
