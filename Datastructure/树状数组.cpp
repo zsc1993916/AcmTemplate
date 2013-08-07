ef4afed30398f193f07927e920968ae9c2f41f12
Scar <565297712@qq.com>
2013-08-07 10:42:01 +0800

    Update 树状数组.cpp

--- Datastructure/树状数组.cpp
+++ Datastructure/树状数组.cpp
@@ -6,14 +6,14 @@ int lowbit(int x) {
 void add(int k){
     while(k < M){
         x[k] ++;
-        k += lowbit[k];
+        k += lowbit(k);
     }
 }
 int sum(int k){
     int res = 0;
     while(k > 0){
         res += x[k];
-        k -= lowbit[k];
+        k -= lowbit(k);
     }
     return res;
 }
