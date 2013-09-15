ded869a4bbcd530fc094262fdba01ae8e2ae3e22
Scar <565297712@qq.com>
2013-09-16 00:03:12 +0800

    Update 常用.cpp

--- Math/常用.cpp
+++ Math/常用.cpp
@@ -1,21 +1,22 @@
 /*
-素数筛法  O(n)
+筛素数 O(n)
 */
+const int N=100;
 bool isPrime[N] ; //数组定义该数字是否为素数
 int prime[N] ; // 存下素数
-int tot = 0 ; //第几个素数
+int total = 0 ; //第几个素数
 
 void seive(){
-        memset( isPrime, 1, sizeof(isPrime));
-        memset( prime , 0, sizeof(prime));
-        isPrime[0] = 0;
-        isPrime[1] = 1;
-        for ( int i=2 ; i<=N; i++) {
-          if (isPrime[i]) prime[++total] = i ;
-          for(int j=1 ; j<=total && i*prime[j]<=Max; j++){
-              isPrime[i*prime[j]] = 0;
-              if (!(i%prime[j])) break;
-            }
+        memset( isPrime , true , sizeof( isPrime ));
+        memset( prime , 0 , sizeof( prime ));
+        isPrime[0 ] = false ;
+        isPrime[1] = false ;
+        for ( int i = 2 ; i <= N ; i++ ){
+              if ( isPrime[i] )  prime[ ++ total ] = i ;
+              for ( int j = 1 ; j <= total && i * prime[j] <= N ; j++) {
+                  isPrime[ i * prime[j] ] = false ;
+                  if (!( i % prime[j])) break;
+              }
         }
 }
 
